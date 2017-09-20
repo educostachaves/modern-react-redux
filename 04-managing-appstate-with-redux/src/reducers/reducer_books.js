75f99bcaff376a4c7653eefefbc9aed5ebfe1528
Eduardo Chaves <educostachaves@gmail.com>
2017-09-20 10:40:24 -0300

    upload section 4

---
+++ 04-managing-appstate-with-redux/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Javascript: The Good Parts', pages: 3101 },
+    { title: 'Harry Potter', pages: 4345 },
+    { title: 'The Dark Tower', pages: 1345 },
+    { title: 'Eloquent Ruby', pages: 1123 }
+  ];
+}
